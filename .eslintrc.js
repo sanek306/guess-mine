beba41d0718bd8119542f7c961ddfa0107d9e245
sanek306 <sanek306@gmail.com>
2020-11-28 21:22:24 +0300

    added start server init socket io

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.export = {
+    env: {
+      browser: true,
+      es6: true,
+      node: true
+    },
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        "no-console": 'off'
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# guess-mine
+# Guess Mine
+
 Realtime Drawing Game with SocketIO, Gulp and Node
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [x] ESLint
+- [x] SocketIO
